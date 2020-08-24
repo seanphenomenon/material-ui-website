27648aaec79d49ebb148db61e189d897b7d3ff4d
seanphenomenon <seanharrell@Seans-MacBook-Pro.local>
2020-08-23 22:08:47 -0700

    finished responsive navbar and mobile drawer menu

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -9,7 +9,15 @@ import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import MenuList from "@material-ui/core/MenuList";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
 
 import logo from "../../assets/logo.svg";
 
@@ -30,10 +38,23 @@ const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar, //using spread operator here copies the styles over from default theme mixin to be able to apply them to components/elements. Helps to push text from under toolbar.
     marginBottom: "3em",
+    [theme.breakpoints.down('md')]: {
+      marginBottom: '2em'
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: '1.25em' 
+  },
   },
   logo: {
     height: "8em",
+    [theme.breakpoints.down('md')]: {
+      height: '7em'
+    },
+    [theme.breakpoints.down('xs')]: {
+      height: '5.5em' 
   },
+  },
+
   logoContainer: {
     padding: 0,
     "&:hover": {
@@ -70,33 +91,74 @@ const useStyles = makeStyles((theme) => ({
       opacity: 1,
     },
   },
+
+  drawerIconContainer: {
+    "&:hover": {
+      backgroundColor: 'transparent'
+    },
+    marginLeft: 'auto'
+  }, 
+
+  drawerIcon: {
+    height: '50px',
+    width: '50px'
+  }, 
+  drawer: {
+     backgroundColor: theme.palette.common.arcBlue,
+  },
+  drawerItem: {
+    ...theme.typography.tab,
+    color: 'white',
+    opacity: '0.7'
+  },
+  drawerItemEstimate: {
+    ...theme.typography.estimate,
+    backgroundColor: theme.palette.common.arcOrange,
+  },
+
+  drawerItemSelected: {
+    opacity: '1'
+  }, 
+
+  appBar: {
+    zIndex: theme.zIndex.modal + 1, // this is created to make app bar appear on top of drawer
+  }
 }));
 
 export default function Header(props) {
   const classes = useStyles();
-  const [value, setValue] = useState(0); // this controls which nav tab we selected
-  const [anchorEl, setAnchorEl] = useState(null); // this stores the drop down services tab menu item we clicked on and what we want to be rendered
-  const [open, setOpen] = useState(false);
-  const [selectedIndex, setSelectedIndex] = useState(0);
+  const theme = useTheme();
+  const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const matches = useMediaQuery(theme.breakpoints.down('md')); //this will select anything that is medium and below to return true. 
 
-  const handleChange = (event, value) => {
-    setValue(value);
+
+// Hooks:
+  const [value, setValue] = useState(0); // this controls which nav tab we selected and its route.
+  const [anchorEl, setAnchorEl] = useState(null); // this stores the drop down services tab menu item we clicked on and what we want to be rendered
+  const [openMenu, setOpenMenu] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [openDrawer, setOpenDrawer] = useState(0);
+
+
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
-    setOpen(true);
+    setOpenMenu(true);
   };
 
   const handleMenuItemClick = (event, index) => {
     setAnchorEl(null);
-    setOpen(false);
+    setOpenMenu(false);
     setSelectedIndex(index);
   };
 
   const handleClose = (event) => {
     setAnchorEl(null);
-    setOpen(false);
+    setOpenMenu(false);
   };
 
   const menuOptions = [
@@ -106,7 +168,7 @@ export default function Header(props) {
     { name: "Website Development", link: "/websites" },
   ];
 
-  // The useEffect lifecycle method is used here to make sure that when the page is refreshed, the nav tab stays on the current tab, and does not default to the 'Home' tab.
+  // The useEffect lifecycle method is used here to make sure that when the page is refreshed, the nav tab stays on the active tab, and does not default to the 'Home' tab.
   useEffect(() => {
     if (window.location.pathname === "/" && value !== 0) {
       setValue(0);
@@ -126,7 +188,7 @@ export default function Header(props) {
     switch (window.location.pathname) {
       case "/":
       if (value !== 0) {
-        setValue(0);
+        setValue(0); 
       }
       break;
       case "/services":
@@ -184,13 +246,157 @@ export default function Header(props) {
       default:
         break;
     }
-  }, [value]); // we need to pass the value array here at the end of the function, to prevent infinite chain of updates, using the state of value depdendencies. If value doesnt change, we do not want to run the useEffect function.
+  }, [value]); // we need to pass the value array here at the end of the function, to prevent infinite chain of updates, using the state of value dependencies. If value doesnt change, we do not want to run the useEffect function.
+
+
+const tabs = (
+  <React.Fragment>
+  <Tabs
+  value={value}
+  onChange={handleChange}
+  className={classes.tabContainer}
+  indicatorColor="primary"
+>
+  <Tab
+    className={classes.tab}
+    component={Link}
+    to="/"
+    value={0}
+    label="Home"
+  />
+  <Tab
+    aria-owns={anchorEl ? "simple-menu" : "undefined"}
+    aria-haspopup={anchorEl ? "true" : "undefined"}
+    onMouseOver={(event) => handleClick(event)}
+    className={classes.tab}
+    component={Link}
+    to="/services"
+    value={1}
+    label="Services"
+  />
+  <Tab
+    className={classes.tab}
+    component={Link}
+    to="/revolution"
+    value={2}
+    label="The Revolution"
+  />
+  <Tab
+    className={classes.tab}
+    component={Link}
+    to="/about"
+    value={3}
+    label="About Us"
+  />
+  <Tab
+    className={classes.tab}
+    component={Link}
+    to="/contact"
+    value={4}
+    label="Contact Us"
+  />
+</Tabs>
+
+<Button
+  variant="contained"
+  color="secondary"
+  className={classes.button}
+  component={Link}
+  to="/estimate"
+  value={5}
+>
+  Free Estimate
+</Button>
+<Menu
+  id="simple-menu"
+  anchorEl={anchorEl}
+  open={openMenu}
+  onClose={handleClose}
+  MenuListProps={{ onMouseLeave: handleClose }}
+  classes={{ paper: classes.menu }} //this is used to customize paper styling background within drop down menu
+  elevation={0}
+  style={{zIndex: 1302}} // makes sure service menu stays on top of appbar. Prevents appbar zindex from covering services menu 
+  
+>
+  {/* <MenuItem onClick={()=> {handleClose(); setValue(1)}} component={Link} to='/services' classes={{root: classes.menuItem}}>Services</MenuItem> created another services menu item here to be able to see nav tab through drop down menu */}
+  {/* <MenuItem onClick={()=> {handleClose(); setValue(1)}} component={Link} to='/customsoftware'  classes={{root: classes.menuItem}} >Custom Software Development</MenuItem>
+  <MenuItem onClick={()=> {handleClose(); setValue(1)}} component={Link} to='/mobileapps'  classes={{root: classes.menuItem}} >Mobile App Development</MenuItem>
+  <MenuItem onClick={()=> {handleClose(); setValue(1)}} component={Link} to='/websites' classes={{root: classes.menuItem}} > Website Development </MenuItem> */}
+
+  
+  
+  {/*array of menu items created below instead to showcase link that is currently active and to refactor MenuItem code above */}
+  {menuOptions.map((option, index) => (
+    <MenuItem
+      key={option}
+      component={Link}
+      to={option.link}
+      classes={{ root: classes.menuItem }}
+      onClick={(event) => {
+        handleMenuItemClick(event, index);
+        setValue(1);
+        handleClose();
+      }}
+      selected={index === selectedIndex && value === 1} //the selected prop allows us to indicate which menu item is currently selected/active
+    >
+      {option.name}
+    </MenuItem>
+  ))}
+</Menu>
+  </React.Fragment>
+);
+
+
+const drawer = (
+  <React.Fragment>
+    <SwipeableDrawer 
+    disableBackdropTransition={!iOS} //this helps optimize for mobile performance
+    disableDiscovery={iOS} // this helps optimize for mobile performance
+    open={openDrawer} 
+    onClose={() => setOpenDrawer(false)} 
+    onOpen={()=> setOpenDrawer(true)} 
+    classes={{paper: classes.drawer }}
+    >
+    <div className={classes.toolbarMargin}/> {/* this is used to help push the drawer menu under the appbar */}
+      <List disablePadding>
+        <ListItem   divider button component={Link} to='/' onClick={()=> {setOpenDrawer(false); setValue(0)}} selected={value === 0}>
+          <ListItemText className={value === 0 ? [classes.drawerItem, classes.drawerItemSelected] : classes.drawerItem} disableTypography>Home</ListItemText>
+        </ListItem>
+
+         <ListItem divider button component={Link} to='/services' onClick={()=> {setOpenDrawer(false); setValue(1)}} selected={value === 1}>
+          <ListItemText className={value === 1 ? [classes.drawerItem, classes.drawerItemSelected] : classes.drawerItem} disableTypography>Services</ListItemText>
+         </ListItem>
+
+          <ListItem  divider button component={Link} to='/revolution' onClick={()=> {setOpenDrawer(false); setValue(2)}} selected={value === 2}>
+            <ListItemText className={value === 2 ? [classes.drawerItem, classes.drawerItemSelected] : classes.drawerItem} disableTypography>The Revolution</ListItemText>
+          </ListItem>
+
+          <ListItem  divider button component={Link} to='/about' onClick={()=> {setOpenDrawer(false); setValue(3)}} selected={value === 3}>
+            <ListItemText className={value === 3 ? [classes.drawerItem, classes.drawerItemSelected] : classes.drawerItem} disableTypography>About Us</ListItemText>
+          </ListItem>
+
+          <ListItem   divider button component={Link} to='/contact' onClick={()=> {setOpenDrawer(false); setValue(4)}} selected={value === 4}>
+            <ListItemText className={value === 4 ? [classes.drawerItem, classes.drawerItemSelected] : classes.drawerItem} disableTypography>Contact Us</ListItemText>
+          </ListItem>
+
+          <ListItem className={classes.drawerItemEstimate} divider button component={Link} to='/estimate' onClick={()=> {setOpenDrawer(false); setValue(5)}} selected={value === 5}>
+            <ListItemText className={value === 5 ? [classes.drawerItem, classes.drawerItemSelected] : classes.drawerItem} disableTypography>Free Estimate</ListItemText>
+          </ListItem>
+      </List>
+    </SwipeableDrawer>
+    <IconButton  className={classes.drawerIconContainer} onClick={() => setOpenDrawer(!openDrawer)} disableRipple>
+      <MenuIcon className={classes.drawerIcon}/>
+    </IconButton>
+  </React.Fragment>
+)
+
+
 
   return (
     <React.Fragment>
       {/* react fragment allows the use of div below without needing enclosing wrapper div element. */}
       <ElevationScroll>
-        <AppBar position="fixed" color="primary">
+        <AppBar position="fixed" color="primary" className={classes.appBar}>
           <Toolbar disableGutters={true}>
             {/* disable gutters gets rid of default padding on the sides of the toolbar*/}
 
@@ -205,96 +411,7 @@ export default function Header(props) {
               <img src={logo} alt="company logo" className={classes.logo} />
             </Button>
 
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              className={classes.tabContainer}
-              indicatorColor="primary"
-            >
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to="/"
-                value={0}
-                label="Home"
-              />
-              <Tab
-                aria-owns={anchorEl ? "simple-menu" : "undefined"}
-                aria-haspopup={anchorEl ? "true" : "undefined"}
-                onMouseOver={(event) => handleClick(event)}
-                className={classes.tab}
-                component={Link}
-                to="/services"
-                value={1}
-                label="Services"
-              />
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to="/revolution"
-                value={2}
-                label="The Revolution"
-              />
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to="/about"
-                value={3}
-                label="About Us"
-              />
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to="/contact"
-                value={4}
-                label="Contact Us"
-              />
-            </Tabs>
-
-            <Button
-              variant="contained"
-              color="secondary"
-              className={classes.button}
-              component={Link}
-              to="/estimate"
-              value={5}
-            >
-              Free Estimate
-            </Button>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{ onMouseLeave: handleClose }}
-              classes={{ paper: classes.menu }} //this is used to customize paper styling background within drop down menu
-              elevation={0}
-            >
-              {/* <MenuItem onClick={()=> {handleClose(); setValue(1)}} component={Link} to='/services' classes={{root: classes.menuItem}}>Services</MenuItem> created another services menu item here to be able to see nav tab through drop down menu */}
-              {/* <MenuItem onClick={()=> {handleClose(); setValue(1)}} component={Link} to='/customsoftware'  classes={{root: classes.menuItem}} >Custom Software Development</MenuItem>
-              <MenuItem onClick={()=> {handleClose(); setValue(1)}} component={Link} to='/mobileapps'  classes={{root: classes.menuItem}} >Mobile App Development</MenuItem>
-              <MenuItem onClick={()=> {handleClose(); setValue(1)}} component={Link} to='/websites' classes={{root: classes.menuItem}} > Website Development </MenuItem> */}
-
-              
-              
-              {/*array of menu items created below instead to showcase link that is currently active and to refactor MenuItem code above */}
-              {menuOptions.map((option, index) => (
-                <MenuItem
-                  key={option}
-                  component={Link}
-                  to={option.link}
-                  classes={{ root: classes.menuItem }}
-                  onClick={(event) => {
-                    handleMenuItemClick(event, index);
-                    setValue(1);
-                    handleClose();
-                  }}
-                  selected={index === selectedIndex && value === 1}
-                >
-                  {option.name}
-                </MenuItem>
-              ))}
-            </Menu>
+            {matches ? drawer: tabs} 
           </Toolbar>
         </AppBar>
       </ElevationScroll>
